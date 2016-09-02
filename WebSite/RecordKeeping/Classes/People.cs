5d615cbdf8b1a1427979cfa2119c6370f1a25cf6
Harshal <hbonde@email.arizona.edu>
2016-09-02 12:04:04 -0700

    I am down with OOP, yea you know me.

--- WebSite/RecordKeeping/Classes/People.cs
+++ WebSite/RecordKeeping/Classes/People.cs
@@ -9,10 +9,24 @@ namespace RecordKeeping.Classes
     public class People
     {
         private List<Person> Peeps { get; set; }
-
+        private Boolean IsValid = true;
+        private String Name;
+        private float AverageAge = 0;
+        private int NumberOfPeople = 0;
         public People(List<Person> peeps )
         {
             Peeps=peeps;
+        }
+        /*Constructor Override*/
+        public People()
+        {
+            List<Person> peepsAlt = new List<Person> ();
+            Person p = new Person();
+            p.FirstName = "Harshal";
+            p.LastName = "Bonde";
+            p.StateCode = "AZ";
+            peepsAlt.Add(p);
+            Peeps = peepsAlt;
         }
 
     }
