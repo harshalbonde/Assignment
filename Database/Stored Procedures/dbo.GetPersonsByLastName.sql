6a5ca4d479e0be2cc67fb0839b16056d7984ab01
Harshal <hbonde@email.arizona.edu>
2016-09-01 19:44:12 -0700

    Last name sp

---
+++ Database/Stored Procedures/dbo.GetPersonsByLastName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROC [dbo].[GetPersonsByLastName] @LastName VARCHAR (50)
+AS
+    SET NOCOUNT ON; 
+
+    SELECT  PersonId ,
+            FirstName ,
+            LastName
+    FROM    dbo.Persons
+    WHERE   LastName = @LastName;
